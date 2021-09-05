c4a746bcfb75189b2c5833352e10180ba64bbebd
Byeongmin <gui549@naver.com>
2021-09-05 17:20:57 +0900

    21.09.05

--- Medium/1881.MaximumValueafterInsertion.py
+++ Medium/1881.MaximumValueafterInsertion.py
@@ -2,9 +2,14 @@ class Solution:
     def maxValue(self, n: str, x: int) -> str:
         if n[0] == '-':
             for i in range(1, len(n)):
-                if n[i] < str(x):
+                if n[i] > str(x):
                     return n[:i] + str(x) + n[i:]
+                
+            return n + str(x)
+        
         else:
             for i in range(len(n)):
                 if n[i] < str(x):
                     return n[:i] + str(x) + n[i:]
+            
+            return n + str(x)
