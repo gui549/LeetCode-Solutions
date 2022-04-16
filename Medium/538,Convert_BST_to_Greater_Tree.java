a00f80cb3c5c6885ea208ec418b828cd99a34acd
Byeongmin Kong <gui549@naver.com>
2022-04-16 14:48:28 +0900

    22.04.16

---
+++ Medium/538,Convert_BST_to_Greater_Tree.java
@@ -0,0 +1,34 @@
+package Medium;
+
+public class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+    TreeNode() {}
+    TreeNode(int val) { this.val = val; }
+    TreeNode(int val, TreeNode left, TreeNode right) {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+}
+
+class Solution {
+    
+    private int sum = 0;
+
+    public TreeNode convertBST(TreeNode root) {
+        if (root == null) return null;
+        if (root.right != null) {
+            convertBST(root.right);
+        }
+
+        sum += root.val;
+        root.val = sum;
+        if (root.left != null) {
+            convertBST(root.left);
+        }
+
+        return root;
+    }
+}
