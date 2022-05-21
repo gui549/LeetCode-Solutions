afa74d2f92d1c6517f7c1fdbfbccda84b4042705
kongbm <gui549@naver.com>
2022-05-22 00:31:45 +0900

    22.05.22

---
+++ Medium/322.Coin_Change.java
@@ -0,0 +1,28 @@
+package Medium;
+
+import java.util.Arrays;
+
+class Solution {
+    public int coinChange(int[] coins, int amount) {
+        if (amount == 0) {
+            return 0;
+        }
+        
+        int[] dp = new int[amount + 1];
+        Arrays.fill(dp, Integer.MAX_VALUE);        
+        dp[0] = 0;
+        for (int i = 0; i < amount + 1; i++) {
+            if (dp[i] == Integer.MAX_VALUE) {
+                continue;
+            }
+
+            for (int coin : coins) {
+                if (0 < i + coin && i + coin < amount + 1) {
+                    dp[i + coin] = Math.min(dp[i + coin], dp[i] + 1);
+                }
+            }
+        }
+        System.out.println(Arrays.toString(dp));       
+        return dp[amount] == Integer.MAX_VALUE ? -1 : dp[amount];
+    }
+}
